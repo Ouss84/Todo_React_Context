832ee62c1f97ef2ba2a33d6220773769149557f5
Ouss84 <bahrioussama84@gmail.com>
2022-02-07 12:12:26 +0200

    linking input fields to the sates + creating the store file for data storage

--- src/components/Todo/AddTodo.js
+++ src/components/Todo/AddTodo.js
@@ -1,19 +1,32 @@
 import React from "react";
 import classes from "./AddTodo.module.css";
 import Button from "../UI/Button";
-
+import { useState } from "react";
 const AddTodo = ({ addHandler }) => {
+  const [userInput, setUserInput] = useState({
+    title: "",
+    task: "",
+  });
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setUserInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(userInput);
+  };
   return (
     <form onSubmit={addHandler} className={classes.input}>
       <div>
-        <label>Title</label>
-        <input type="text" />
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" onChange={inputHandler} />
       </div>
       <div>
-        <label>Task</label>
-        <input type="text" />
+        <label htmlFor="task">Task</label>
+        <input type="text" id="task" name="task" onChange={inputHandler} />
       </div>
-      <Button>Add Task</Button>
+      <Button type="submit">Add Task</Button>
     </form>
   );
 };
---
+++ src/components/store/todoStore.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const intialState = {
+  notes: [
+    {
+      id: 1,
+      title: "Create clean app",
+      task: "npx create-react-app",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Clean app",
+      task: "Delete and clean unnecessary stuff",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "Create store / context",
+      task: "Create new file and use React.createContext()",
+      done: false,
+    },
+  ],
+};
